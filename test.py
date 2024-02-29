450a21f13a47090b5aa5a742c763eb12155a97d3
esspee <shadowphoenix39@gmail.com>
2024-02-29 10:39:32 -0500

    get API requests working

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("test")
+import requests
+import json
+
+response = requests.get('https://api.scryfall.com/cards/search?q=mulldrifter+set%3ASLD')
+
+data = response.json()
+
+json_formatted_str = json.dumps(data, indent=2)
+
+print(json_formatted_str)
