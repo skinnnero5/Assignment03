5126a39e0effb21d5eb3a2d7c4655755bc436d92
esspee <shadowphoenix39@gmail.com>
2024-02-29 17:44:50 -0500

    add a few variables to make it cleaner

--- test.py
+++ test.py
@@ -2,28 +2,33 @@ import requests
 import json
 import redis
 
-response = requests.get('https://api.scryfall.com/cards/search?q=mulldrifter+set%3ASLD')
+set_code = 'RVR'
+
+response = requests.get('https://api.scryfall.com/cards/search?q=set%3A' + set_code + '+rarity>%3DR')
 
 data = response.json()
 
 json_formatted_str = json.dumps(data, indent=2)
 
-print(json_formatted_str)
+#print(json_formatted_str)
 
 import pandas as pd
 from db_config import get_redis_connection
 
 r = get_redis_connection()
 
-## r.flushall()
+r.flushall()
 
-r.json().set('cards:blue:mulldrifer', '.', json.dumps(data))
+for card in data['data']:
+    name = card['name']
+    r.json().set('cards:' + set_code + ':' + name, '.', json.dumps(card))
+    print(f"Name: {name}")
 
 # Get JSON and decode 
-json_data = r.json().get('cards:blue:mulldrifer')
+json_data = r.json().get('cards:' + set_code + ':' + 'Karn, the Great Creator',)
 data2 = json.loads(json_data)
 
 # Extract fields  
-name = data2['data'][0]['name']
+name = data2['type_line']
+print(name)
 
-print(f"Name: {name}")
