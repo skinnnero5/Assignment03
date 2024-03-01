ff07a74d718b38aecb3a0ce9855883f67a8e06ea
esspee <shadowphoenix39@gmail.com>
2024-03-01 13:48:12 -0500

    introduce RedisInterface class to divide code more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from scryfall_fetcher import ScryfallFetcher
+from redis_interface import RedisInterface
 import json
-import redis
 
 set = 'RVR'
 search_params = {'rarity': 'M'}
@@ -10,22 +10,15 @@ cards = ScryfallFetcher.fetch_cards(set, search_params)
 for card in cards:
     print(card['name'])
 
-import pandas as pd
-from db_config import get_redis_connection
+redis_interface = RedisInterface()
 
-r = get_redis_connection()
+redis_interface.flush()
 
-r.flushall()
-
-for card in cards:
-    name = card['name']
-    r.json().set('cards:' + set + ':' + name, '.', json.dumps(card))
-    print(f"Name: {name}")
+redis_interface.insert_cards(set, cards)
 
 # Get JSON and decode 
-json_data = r.json().get('cards:' + set + ':' + 'Karn, the Great Creator',)
-data2 = json.loads(json_data)
+json_data = redis_interface.get_cards(set, 'Karn, the Great Creator',)
+data = json.loads(json_data)
 
 # Extract fields  
-name = data2['type_line']
-print(name)
+print(data['type_line'])
---
+++ redis_interface.py
@@ -0,0 +1,43 @@
+from db_config import get_redis_connection
+import json
+
+class RedisInterface:
+    """Class for inserting into and querying the RedisJSON database."""
+
+    def __init__(self):
+        """
+        Initializes the RedisInterface based on config.yaml.
+        """
+        self.r = get_redis_connection()
+
+    def insert_cards(self, set, cards):
+        """
+        Inserts JSONs into RedisJSON given a set and a list of cards.
+
+        Parameters:
+        - set (str): The Magic the Gathering set abbreviation (ex: 'RVR' for Ravnica Remastered).
+        - cards (list): List of cards in JSON format from Scryfall API.
+
+        Returns:
+        - result: 0 if successful, 1 if error.
+        """
+        for card in cards:
+            name = card['name']
+            self.r.json().set('cards:' + set + ':' + name, '.', json.dumps(card))
+            print(f"Name: {name}")
+
+    def get_cards(self, set, name = None):
+        """Retrieves cards from database, default all from a set, or selected by name."""
+
+        if name:
+            json_data = self.r.json().get('cards:' + set + ':' + name,)
+        else:
+            json_data = self.r.json().get('cards:' + set,)
+
+        return json_data
+    
+    def flush(self):
+        """Flushes database."""
+        self.r.flushall()
+
+        
